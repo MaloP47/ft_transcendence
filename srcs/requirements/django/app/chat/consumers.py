9ae056cce5d4c21f10f24ac0f7332b0c9441c3f6
kscarmy <guderram@student.42.fr>
2024-06-01 23:46:16 +0200

    push 22 : Correction du probleme de login dans le chat.

--- srcs/requirements/django/app/chat/consumers.py
+++ srcs/requirements/django/app/chat/consumers.py
@@ -24,26 +24,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        username = self.scope['user'].username
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user': username,
+                'timestamp': str(date.today())
             }
         )
 
-    # async def chat_message(self, event):
-    #     message = event['message']
 
-    #     await self.send(text_data=json.dumps({
-    #         'message': message
-    #     }))
 
     async def chat_message(self, event):
         message = event['message']
-        username = self.scope['user'].username
-        timestamp = str(date.today())
+        username = event['user']
+        timestamp = event['timestamp']
         
         # Envoyer le message au WebSocket
         await self.send(text_data=json.dumps({
