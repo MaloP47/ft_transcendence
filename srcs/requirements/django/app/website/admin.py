0c01af9ba468e72024839da4b91420c91c980713
Guillaume Brunet <gbrunet@student.42.fr>
2024-06-07 12:54:50 +0200

    friend request WIP. TODO : alert when friend request sended until someone delete the request

--- srcs/requirements/django/app/website/admin.py
+++ srcs/requirements/django/app/website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from website.models import User, Room, Message
+from website.models import FriendRequest, User, Room, Message
 
 admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Message)
+admin.site.register(FriendRequest)
--- srcs/requirements/django/app/website/urls.py
+++ srcs/requirements/django/app/website/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	path('api/user/signin/', views.signinUser, name="signin"),
 	path('api/user/search/', views.searchUser, name="searchUser"),
 	path('api/user/addfriend/', views.addFriend, name="addFriend"),
+	path('api/user/acceptfriend/', views.acceptFriend, name="acceptFriend"),
 	path('api/user/', views.getUser, name="getUser"),
 	path('api/view/profilMenu/', views.profilMenu, name="profilMenu"),
 	path('api/view/login/', views.loginForm, name="loginForm"),
