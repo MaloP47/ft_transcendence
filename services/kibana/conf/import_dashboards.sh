6dab2046d944ecbe051fe18c728e122ef3afc27c
kscarmy <guderram@student.42.fr>
2024-07-08 11:50:55 +0200

    Push 35 : Cest bon on a notre Index patterns qui est bien ajoute.

--- services/kibana/conf/import_dashboards.sh
+++ services/kibana/conf/import_dashboards.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Démarrer Kibana en arrière-plan
-exec kibana
+echo "Starting Kibana..."
 
+# exec kibana
+/usr/share/kibana/bin/kibana --allow-root &
+# exec kibana --allow-root
+
+
+echo "Kibana started."
 
 # Attendre que Kibana soit prêt
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5601/api/status)" != "200" ]]; do 
@@ -17,16 +23,23 @@ if curl -s -X GET "localhost:5601/api/saved_objects/_find?type=index-pattern&sea
 		-H 'kbn-xsrf: true' \
 		-d '{"attributes":{"title":"filebeat-*","timeFieldName":"@timestamp"}}'
 	echo "Index pattern 'filebeat-*' added."
+	# exec kibana --allow-root
 else
 	echo "Index pattern 'filebeat-*' already exists."
 fi
 
-# Vérifier si le tableau de bord existe déjà
-if curl -s -X GET "localhost:5601/api/saved_objects/_find?type=dashboard&search=Mon-Tableau" | grep -q '"total":0'; then
-	# Importer le tableau de bord
-	curl -X POST "http://kibana:5601/api/saved_objects/_import" \
-	-H "kbn-xsrf: true" \
-	--form file=@/usr/share/kibana/dashboard.json
-else
-	echo "Dashboard 'My Filebeat Dashboard' already exists."
-fi
+sleep 5;
+
+kill $(ps aux | grep 'kibana' | awk '{print $2}')
+
+exec kibana --allow-root
+
+# # Vérifier si le tableau de bord existe déjà
+# if curl -s -X GET "localhost:5601/api/saved_objects/_find?type=dashboard&search=Mon-Tableau" | grep -q '"total":0'; then
+# 	# Importer le tableau de bord
+# 	curl -X POST "http://kibana:5601/api/saved_objects/_import" \
+# 	-H "kbn-xsrf: true" \
+# 	--form file=@/usr/share/kibana/dashboard.json
+# else
+# 	echo "Dashboard 'My Filebeat Dashboard' already exists."
+# fi
