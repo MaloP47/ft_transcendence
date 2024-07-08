8f20301c5d5387d19ab7ed34acfbb21fc0b1aa9c
Guillaume Derramond <guderram@student.42.fr>
2024-07-08 05:50:15 +0200

    Push 32 : Push avant tests pour mettre un dashboard fonctionnel...

--- services/kibana/conf/import_dashboards.sh
+++ services/kibana/conf/import_dashboards.sh
@@ -10,7 +10,6 @@ while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5601/api/status)"
 	sleep 5
 done
 
-# Vérifier si l'index pattern existe déjà
 if curl -s -X GET "localhost:5601/api/saved_objects/_find?type=index-pattern&search=filebeat-*&search_fields=title" | grep -q '"total":0'; then
 	# Ajouter l'index pattern
 	curl -X POST "localhost:5601/api/saved_objects/index-pattern" \
@@ -20,4 +19,14 @@ if curl -s -X GET "localhost:5601/api/saved_objects/_find?type=index-pattern&sea
 	echo "Index pattern 'filebeat-*' added."
 else
 	echo "Index pattern 'filebeat-*' already exists."
+fi
+
+# Vérifier si le tableau de bord existe déjà
+if curl -s -X GET "localhost:5601/api/saved_objects/_find?type=dashboard&search=Mon-Tableau" | grep -q '"total":0'; then
+	# Importer le tableau de bord
+	curl -X POST "http://kibana:5601/api/saved_objects/_import" \
+	-H "kbn-xsrf: true" \
+	--form file=@/usr/share/kibana/dashboard.json
+else
+	echo "Dashboard 'My Filebeat Dashboard' already exists."
 fi
