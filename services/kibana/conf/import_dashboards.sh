c505adcb941e405751a0063886831a870de93cf5
kscarmy <guderram@student.42.fr>
2024-07-20 22:51:12 +0200

    Push 46 : Alors, il faut tout verifier, mais a priori ca fonctionne.

--- services/kibana/conf/import_dashboards.sh
+++ services/kibana/conf/import_dashboards.sh
@@ -9,17 +9,17 @@ KIBANA_PID=$!
 
 echo "Kibana started with PID $KIBANA_PID."
 
-# Attendre que Kibana soit prêt
-# while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5601/api/status)" != "200" ]]; do 
-# 	echo "Waiting for Kibana to be available..."
-# 	sleep 5
-# done
+Attendre que Kibana soit prêt
+while [[ "$(curl -u elastic:DidierDidier -s -o /dev/null -w ''%{http_code}'' localhost:5601/api/status)" != "200" ]]; do 
+	echo "Waiting for Kibana to be available..."
+	sleep 5
+done
 
-sleep 5
+# sleep 5
 
-if curl -s -X GET "localhost:5601/api/saved_objects/_find?type=index-pattern&search=filebeat-*&search_fields=title" | grep -q '"total":0'; then
+if curl -u elastic:DidierDidier -s -X GET "localhost:5601/api/saved_objects/_find?type=index-pattern&search=filebeat-*&search_fields=title" | grep -q '"total":0'; then
 	# Ajouter l'index pattern
-	curl -X POST "localhost:5601/api/saved_objects/index-pattern" \
+	curl -u elastic:DidierDidier  -X POST "localhost:5601/api/saved_objects/index-pattern" \
 		-H 'Content-Type: application/json' \
 		-H 'kbn-xsrf: true' \
 		-d '{"attributes":{"title":"filebeat-*","timeFieldName":"@timestamp"}}'
@@ -29,9 +29,9 @@ else
 fi
 
 # Vérifier si le tableau de bord existe déjà
-if curl -s -X GET "localhost:5601/api/saved_objects/_find?type=dashboard&search=Mon-Beau-Tableau" | grep -q '"total":0'; then
+if curl -u elastic:DidierDidier  -s -X GET "localhost:5601/api/saved_objects/_find?type=dashboard&search=Mon-Beau-Tableau" | grep -q '"total":0'; then
 	# curl -X POST "http://localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@/usr/share/kibana/dashboard.ndjson
-	curl -X POST "http://localhost:5601/api/saved_objects/_import?overwrite=true" \
+	curl -u elastic:DidierDidier -X POST "http://localhost:5601/api/saved_objects/_import?overwrite=true" \
 	-H "kbn-xsrf: true" \
 	--form file=@/usr/share/kibana/dashboard.ndjson
 	echo "Dashboard 'Mon-Beau-Tableau' added."
