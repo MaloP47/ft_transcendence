f5ba865f441a1de37b5ccc11ed9b3a06c480d8ce
kscarmy <guderram@student.42.fr>
2024-07-21 13:57:58 +0200

    Push 48 : Les fichiers sont clean. il reste a tout migrer dans le env...

--- services/kibana/conf/import_dashboards.sh
+++ services/kibana/conf/import_dashboards.sh
@@ -15,19 +15,6 @@ while [[ "$(curl -u elastic:DidierDidier -s -o /dev/null -w ''%{http_code}'' loc
 	sleep 5
 done
 
-# sleep 5
-
-# if curl -u elastic:DidierDidier -s -X GET "localhost:5601/api/saved_objects/_find?type=index-pattern&search=filebeat-*&search_fields=title" | grep -q '"total":0'; then
-# 	# Ajouter l'index pattern
-# 	curl -u elastic:DidierDidier  -X POST "localhost:5601/api/saved_objects/index-pattern" \
-# 		-H 'Content-Type: application/json' \
-# 		-H 'kbn-xsrf: true' \
-# 		-d '{"attributes":{"title":"filebeat-*","timeFieldName":"@timestamp"}}'
-# 	echo "Index pattern 'filebeat-*' added."
-# else
-# 	echo "Index pattern 'filebeat-*' already exists."
-# fi
-
 if curl -u elastic:DidierDidier -s -X GET "localhost:5601/api/saved_objects/_find?type=index-pattern&search=logs*&search_fields=title" | grep -q '"total":0'; then
 	# Ajouter l'index pattern
 	curl -u elastic:DidierDidier  -X POST "localhost:5601/api/saved_objects/index-pattern" \
@@ -41,7 +28,6 @@ fi
 
 # Vérifier si le tableau de bord existe déjà
 if curl -u elastic:DidierDidier  -s -X GET "localhost:5601/api/saved_objects/_find?type=dashboard&search=Mon-Beau-Tableau" | grep -q '"total":0'; then
-	# curl -X POST "http://localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@/usr/share/kibana/dashboard.ndjson
 	curl -u elastic:DidierDidier -X POST "http://localhost:5601/api/saved_objects/_import?overwrite=true" \
 	-H "kbn-xsrf: true" \
 	--form file=@/usr/share/kibana/dashboard.ndjson
